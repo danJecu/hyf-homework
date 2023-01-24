073f1ddff0beda76af42bfbcdf52931ed41835a1
danJecu <jdanalexandru@gmail.com>
2023-01-24 17:57:07 +0100

    Final commit

--- javascript/javascript3/week2/currency-calculator-app/script.js
+++ javascript/javascript3/week2/currency-calculator-app/script.js
@@ -7,58 +7,68 @@ const result = document.querySelector('#result');
 loadCurrencies();
 
 function handleSelectedText(selection) {
-  if (selection.selectedIndex === -1) {
-    return null;
-  }
-  return selection.options[selection.selectedIndex].text;
+    if (selection.selectedIndex === -1) {
+        return null;
+    }
+    return selection.options[selection.selectedIndex].text;
 }
 
 // Convert currency when button is clicked
 async function convertCurrency() {
-  const currencyFrom = handleSelectedText(fromCurrency);
-  const currencyTo = handleSelectedText(toCurrency);
-  const response = await fetch(
-    `https://open.er-api.com/v6/latest/${currencyFrom}`
-  );
-  const data = await response.json();
-  const exchangeRate = await data.rates[currencyTo];
-  return (amount.value * exchangeRate).toFixed(2);
-}
-
-async function loadCurrencies(currency = 'EUR') {
-  try {
-    fromCurrency.innerHTML = '';
-    toCurrency.innerHTML = '';
+    const currencyFrom = handleSelectedText(fromCurrency);
+    const currencyTo = handleSelectedText(toCurrency);
     const response = await fetch(
-      `https://open.er-api.com/v6/latest/${currency}`
+        `https://open.er-api.com/v6/latest/${currencyFrom}`
     );
     const data = await response.json();
-    const currencies = Object.keys(data.rates);
-    // Populate options for select elements
-    currencies.forEach((currency) => {
-      const option = document.createElement('option');
-      option.value = data.rates[currency];
-      option.text = currency;
-      fromCurrency.appendChild(option);
-      const option2 = document.createElement('option');
-      option2.value = currency;
-      option2.text = currency;
-      toCurrency.appendChild(option2);
-    });
+    const exchangeRate = await data.rates[currencyTo];
+    const totalAmount = amount.value;
 
-    toCurrency.value = 'DKK';
-  } catch (err) {
-    console.log(err);
-  }
+    return (totalAmount * exchangeRate).toFixed(2);
+}
+
+// Displaying the currency selections
+async function loadCurrencies(currency = 'EUR') {
+    try {
+        fromCurrency.innerHTML = '';
+        toCurrency.innerHTML = '';
+        const response = await fetch(
+            `https://open.er-api.com/v6/latest/${currency}`
+        );
+        const data = await response.json();
+        const currencies = Object.keys(data.rates);
+        // Populate options for select elements
+        currencies.forEach(currency => {
+            const option = document.createElement('option');
+
+            option.value = data.rates[currency];
+            option.text = currency;
+            fromCurrency.appendChild(option);
+            const option2 = document.createElement('option');
+            option2.value = currency;
+            option2.text = currency;
+            toCurrency.appendChild(option2);
+        });
+
+        toCurrency.value = 'DKK';
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 fromCurrency.addEventListener('change', async () => {
-  const currency = handleSelectedText(fromCurrency);
-  loadCurrencies(currency);
+    const currency = handleSelectedText(fromCurrency);
+    loadCurrencies(currency);
 });
 
-button.addEventListener('click', async (e) => {
-  e.preventDefault();
-  const convertedAmount = await convertCurrency();
-  result.innerText = convertedAmount;
+button.addEventListener('click', async e => {
+    e.preventDefault();
+    const convertedAmount = await convertCurrency();
+
+    if (convertedAmount === '0.00') {
+        result.innerText = 'Enter an amount!';
+    } else {
+        result.innerText =
+            convertedAmount + ' ' + handleSelectedText(toCurrency);
+    }
 });
