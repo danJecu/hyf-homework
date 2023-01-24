413e6cf980c634f90bc4ca6320313f8cb58d5f9d
danJecu <jdanalexandru@gmail.com>
2023-01-24 13:14:27 +0100

    Finished the logic for the project

--- javascript/javascript3/week2/currency-calculator-app/script.js
+++ javascript/javascript3/week2/currency-calculator-app/script.js
@@ -2,16 +2,33 @@ const amount = document.querySelector('#amount');
 const fromCurrency = document.querySelector('#from-currency');
 const toCurrency = document.querySelector('#to-currency');
 const button = document.querySelector('#convert');
+const result = document.querySelector('#result');
 
 loadCurrencies();
 
+function handleSelectedText(selection) {
+  if (selection.selectedIndex === -1) {
+    return null;
+  }
+  return selection.options[selection.selectedIndex].text;
+}
+
 // Convert currency when button is clicked
-function convertCurrency() {
-  console.log(`${amount}-${fromCurrency}-${toCurrency}`);
+async function convertCurrency() {
+  const currencyFrom = handleSelectedText(fromCurrency);
+  const currencyTo = handleSelectedText(toCurrency);
+  const response = await fetch(
+    `https://open.er-api.com/v6/latest/${currencyFrom}`
+  );
+  const data = await response.json();
+  const exchangeRate = await data.rates[currencyTo];
+  return (amount.value * exchangeRate).toFixed(2);
 }
 
 async function loadCurrencies(currency = 'DKK') {
   try {
+    fromCurrency.innerHTML = '';
+    toCurrency.innerHTML = '';
     const response = await fetch(
       `https://open.er-api.com/v6/latest/${currency}`
     );
@@ -35,6 +52,13 @@ async function loadCurrencies(currency = 'DKK') {
   }
 }
 
-fromCurrency.addEventListener('change', () => {
-  loadCurrencies();
+fromCurrency.addEventListener('change', async () => {
+  const currency = handleSelectedText(fromCurrency);
+  loadCurrencies(currency);
+});
+
+button.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const convertedAmount = await convertCurrency();
+  result.innerText = convertedAmount;
 });
