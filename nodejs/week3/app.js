b750b50bccc23d60da47e4ee234bcf347230bc6a
danJecu <jdanalexandru@gmail.com>
2023-02-23 04:46:18 +0100

    Added env file and removed files from commited in the wrong folder

--- nodejs/week3/app.js
+++ nodejs/week3/app.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host: process.env.DB_HOST || '0.0.0.0',
-        port: process.env.DB_PORT || 3306,
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '1234',
-        database: process.env.DB_NAME || 'hyf_node_week3_warmup',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         multipleStatements: true,
     },
 });
