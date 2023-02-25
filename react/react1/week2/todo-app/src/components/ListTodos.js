1d9ec4a533f7db924b5746fe4e7f2ad8e8738c5f
Dan Jecu <jdanalexandru@gmail.com>
2023-02-25 20:12:44 +0100

    moved the props deconstruction in todo component

--- react/react1/week2/todo-app/src/components/ListTodos.js
+++ react/react1/week2/todo-app/src/components/ListTodos.js
@@ -1,16 +1,11 @@
 import Todo from './Todo';
 
 export default function ListToDos({ todos, handleDelete }) {
-  return (
-    <ul className="todos-container">
-      {todos.map((todo) => (
-        <Todo
-          key={todo.id}
-          id={todo.id}
-          description={todo.description}
-          handleDelete={handleDelete}
-        />
-      ))}
-    </ul>
-  );
+    return (
+        <ul className='todos-container'>
+            {todos.map(todo => (
+                <Todo todo={todo} handleDelete={handleDelete} key={todo.id} />
+            ))}
+        </ul>
+    );
 }
--- react/react1/week2/todo-app/src/components/Todo.js
+++ react/react1/week2/todo-app/src/components/Todo.js
@@ -1,24 +1,24 @@
 import { useState } from 'react';
 
-export default function Todo({ description, id, handleDelete }) {
-  const [checked, setChecked] = useState(false);
+export default function Todo({ todo, handleDelete }) {
+    const [checked, setChecked] = useState(false);
 
-  return (
-    <li className="todo-item">
-      <span
-        style={{
-          textDecoration: checked ? 'line-through' : 'none',
-        }}
-      >
-        {description}
-      </span>
+    return (
+        <li className='todo-item' id={todo.id}>
+            <span
+                style={{
+                    textDecoration: checked ? 'line-through' : 'none',
+                }}
+            >
+                {todo.description}
+            </span>
 
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={() => (checked ? setChecked(false) : setChecked(true))}
-      />
-      <button onClick={() => handleDelete(id)}>Delete</button>
-    </li>
-  );
+            <input
+                type='checkbox'
+                checked={checked}
+                onChange={() => setChecked(!checked)}
+            />
+            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+        </li>
+    );
 }
