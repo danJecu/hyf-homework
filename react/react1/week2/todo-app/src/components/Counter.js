9551e58fbffb4e8164a341bc0be7ad5c179152e4
Dan Jecu <jdanalexandru@gmail.com>
2023-02-26 18:42:40 +0100

    Final commit

--- react/react1/week2/todo-app/src/components/Counter.js
+++ react/react1/week2/todo-app/src/components/Counter.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 
 export default function Counter() {
-  const [seconds, setSeconds] = useState(0);
+    const [seconds, setSeconds] = useState(0);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setSeconds(seconds + 1);
-    }, 1000);
-  });
+    useEffect(() => {
+        setTimeout(() => {
+            setSeconds(seconds + 1);
+        }, 1000);
+    });
 
-  return <h5>You have spent {seconds} seconds on the website.</h5>;
+    return <h5>You have spent {seconds} seconds on this website.</h5>;
 }
