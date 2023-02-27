93359e531cbca076fdccca9b85efb0f89eab3da3
Dan Jecu <jdanalexandru@gmail.com>
2023-02-27 16:30:11 +0100

    Refactored the search and display functions

--- javascript/javascript3/week1/giphy-app/script.js
+++ javascript/javascript3/week1/giphy-app/script.js
@@ -3,7 +3,7 @@ const container = document.getElementById('grid-container');
 const inputWord = document.getElementById('search');
 const inputLimit = document.getElementById('limit');
 
-async function searchGif(searchWord, limitSearch = 5) {
+async function searchAndDisplayGif(searchWord, limitSearch = 5) {
     try {
         container.innerHTML = '';
         const response = await fetch(
@@ -11,28 +11,23 @@ async function searchGif(searchWord, limitSearch = 5) {
         );
         const data = await response.json();
 
-        return data.data;
+        if (data.data.length > 0) {
+            data.data.forEach(gif => {
+                const img = document.createElement('img');
+                const containerImg = document.createElement('div');
+                containerImg.classList.add('container-img');
+                img.src = gif.images.downsized.url;
+                img.alt = gif.title;
+                containerImg.style.height = gif.images.downsized.height + 'px';
+                containerImg.style.width = gif.images.downsized.width + 'px';
+                containerImg.appendChild(img);
+                container.appendChild(containerImg);
+            });
+        } else {
+            container.innerHTML = '<h3>No GIFs found</h3>';
+        }
     } catch (err) {
         console.log(err);
-    }
-}
-
-function displayGif(results) {
-    container.innerHTML = '';
-    if (results.length > 0) {
-        results.forEach(gif => {
-            const img = document.createElement('img');
-            const containerImg = document.createElement('div');
-            containerImg.classList.add('container-img');
-            img.src = gif.images.downsized.url;
-            img.alt = gif.title;
-            containerImg.style.height = gif.images.downsized.height + 'px';
-            containerImg.style.width = gif.images.downsized.width + 'px';
-            containerImg.appendChild(img);
-            container.appendChild(containerImg);
-        });
-    } else {
-        container.innerHTML = '<h3>No GIFs found</h3>';
     }
 }
 
@@ -42,11 +37,9 @@ document.querySelectorAll('.search-element').forEach(input => {
         const limitGifs = inputLimit.value;
 
         if (limitGifs === '') {
-            const results = await searchGif(searchWord);
-            displayGif(results);
+            await searchAndDisplayGif(searchWord);
         } else {
-            const results = await searchGif(searchWord, limitGifs);
-            displayGif(results);
+            await searchAndDisplayGif(searchWord, limitGifs);
         }
     });
 });
